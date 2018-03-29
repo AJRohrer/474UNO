9c42b0b670b91f6fb6ae5ff7749bc7359bada7d3
Andrew Rohrer <Andrew.Rohrer@uline.com>
2018-03-29 11:13:02 -0500

    Add player functionality

--- src/code/UNOClasses/Player.java
+++ src/code/UNOClasses/Player.java
@@ -1,6 +1,29 @@
 package code.UNOClasses;
 
 
+import code.UNOClasses.Card.UNOCard;
+import code.UNOClasses.Hand.PlayerHand;
+
 public class Player {
     private boolean isHuman; //returns true for the interfacing user, false for computer players (AI)
+    private PlayerHand _hand;
+
+    public Player (boolean isHumanPlayer){
+        this.isHuman = isHumanPlayer;
+    }
+
+    public boolean isHuman() {
+        return isHuman;
+    }
+
+    public void addCardtoHand(UNOCard uc){
+        _hand.addUNOCard(uc);
+    }
+
+    public UNOCard discardCard(UNOCard uc){
+        _hand.removeUNOCard(uc);
+        return uc;
+    }
+
+    public String callUNO(){ return "UNO!"; }
 }
