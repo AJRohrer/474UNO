ae1dabc8f2444ca3d2667703c3b6590b0a6c0890
sizeablethink <sink217@gmail.com>
2018-04-15 18:40:11 -0500

    Added number of player button functionality

--- src/code/UNOClasses/UNOTableController.java
+++ src/code/UNOClasses/UNOTableController.java
@@ -3,6 +3,7 @@ package code.UNOClasses;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 
@@ -12,12 +13,14 @@ import java.util.ResourceBundle;
 public class UNOTableController implements Initializable {
 
     @FXML private Spinner numberOfPlayersSpinner;
-    @FXML private Button setTableButton;
+    @FXML private Button setNumberOfPlayersButton;
+    @FXML private Label numberOfPlayersLabel;
 
     private int numberOfCompPlayers;
 
     public void setNumberOfCompPlayers() {
-
+        this.numberOfCompPlayers = this.numberOfPlayersSpinner.getValue().hashCode();
+        numberOfPlayersLabel.setText("You have chosen " + numberOfCompPlayers + " computer players.");
     }
 
     @Override
@@ -26,9 +29,12 @@ public class UNOTableController implements Initializable {
         SpinnerValueFactory<Integer> playerNumberFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 9 ,1);
         this.numberOfPlayersSpinner.setValueFactory(playerNumberFactory);
         //numberOfPlayersSpinner.setEditable(true);
+
+        numberOfPlayersLabel.setText("");
     }
 
     public void getNumberOfCompPlayers () {
-        //this.numberOfCompPlayers = this.numberOfPlayersSpinner.;
+        this.numberOfCompPlayers = this.numberOfPlayersSpinner.getValue().hashCode();
+        System.out.println(numberOfCompPlayers);
     }
 }
