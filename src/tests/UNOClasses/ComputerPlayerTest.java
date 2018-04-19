afdfbfccaa2c5255f1cdfc4720c1c9a868a4d0f4
Darya Kiktenko <dkiktenko@hotmail.com>
2018-04-18 19:58:49 -0500

    Completed testMakeMove1() in ComputerPlayerTest

--- src/tests/UNOClasses/ComputerPlayerTest.java
+++ src/tests/UNOClasses/ComputerPlayerTest.java
@@ -4,15 +4,15 @@ import code.UNOClasses.Card.CardType;
 import code.UNOClasses.Card.UNOCard;
 import code.UNOClasses.Card.UNOColor;
 import code.UNOClasses.ComputerPlayer;
-import code.UNOClasses.Game;
 import code.UNOClasses.Player;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ComputerPlayerTest {
 
     @Test
-    public void testMakeMove(){
+    public void testMakeMove1(){
         ComputerPlayer AIPlayer = new ComputerPlayer();
         Player playerBefore = new Player(true);
         Player playerAfter = new Player(true);
@@ -20,22 +20,21 @@ public class ComputerPlayerTest {
 
         AIPlayer.myHand().addUNOCard(new UNOCard(CardType.FIVE,UNOColor.GREEN));
         AIPlayer.myHand().addUNOCard(new UNOCard(CardType.REVERSE, UNOColor.YELLOW));
-        AIPlayer.myHand().addUNOCard(new UNOCard(CardType.WILDDRAW4, UNOColor.YELLOW));
+        AIPlayer.myHand().addUNOCard(new UNOCard(CardType.WILD, UNOColor.YELLOW));
         AIPlayer.myHand().addUNOCard(new UNOCard(CardType.ONE, UNOColor.RED));
         AIPlayer.myHand().addUNOCard(new UNOCard(CardType.ONE, UNOColor.GREEN));
 
         for (int i = 0; i <= 6; i++){
             playerAfter.myHand().addUNOCard(null);
-            if (i <= 3){
+            if (i < 3){
                 playerBefore.myHand().addUNOCard(null);
                 playerAfterNext.myHand().addUNOCard(null);
             }
         }
 
         UNOCard playedCard = AIPlayer.makeMove(new UNOCard(CardType.EIGHT,UNOColor.BLUE), playerBefore, playerAfter, playerAfterNext);
-        System.out.println(playedCard.toString());
-
-
+        assertEquals(CardType.WILD, playedCard.get_type());
+        assertEquals(UNOColor.YELLOW, playedCard.get_color());
     }
 
 
